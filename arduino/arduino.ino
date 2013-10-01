97f6e6bcffa327f12258823a5b0acdc0bc776343
fabien <fdepraetre@enseirb-matmeca.fr>
2013-10-01 18:54:19 +0200

    Ajout du code de l'arduino

---
+++ arduino/arduino.ino
@@ -0,0 +1,59 @@
+
+int leftMode = 1;
+int rightMode = 1;
+int leftPWM = 0;
+int rightPWM = 0;
+
+// Initialisation
+
+void setup()
+{
+  pinMode(9 , OUTPUT);
+  pinMode(7 , OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(8 , OUTPUT);
+
+  Serial.begin(115200);          // Baudrate = 115200
+  Serial.flush();  // Flush buffer
+  
+  Serial.write('O');
+}
+
+void loop()
+{
+  
+  SCmode();
+  
+  digitalWrite(7,  leftMode);
+  analogWrite (9,  leftPWM);
+  digitalWrite(8,  rightMode);
+  analogWrite (10, rightPWM);
+  
+  //  
+  //  if(leftPWM > 0)
+  //    leftPWM--;
+  //  if(rightPWM >0)
+  //    rightPWM--;  
+}
+
+
+
+// Discution for serial communication
+
+void SCmode()
+{
+  if( Serial.available()>=5)
+  {
+    int serialSecurity = Serial.read();
+    if( serialSecurity == '$')
+    {
+
+      leftMode  = Serial.read();
+      leftPWM   = Serial.read();
+      rightMode = Serial.read();
+      rightPWM  = Serial.read();    
+    }
+
+  }
+}
+
