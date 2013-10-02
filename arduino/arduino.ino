fe24bdc7c34e1e6589a763b8e0f319f0e8dc7dfa
David <david.bitonneau@gmail.com>
2013-10-02 08:54:33 +0200

    Reindenting arduino file.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,59 +1,54 @@
 
-int leftMode = 1;
+int leftMode  = 1;
 int rightMode = 1;
-int leftPWM = 0;
-int rightPWM = 0;
+int leftPWM   = 0;
+int rightPWM  = 0;
 
-// Initialisation
-
+// Initialization
 void setup()
 {
-  pinMode(9 , OUTPUT);
-  pinMode(7 , OUTPUT);
+  pinMode( 9, OUTPUT);
+  pinMode( 7, OUTPUT);
   pinMode(10, OUTPUT);
-  pinMode(8 , OUTPUT);
+  pinMode( 8, OUTPUT);
 
   Serial.begin(115200);          // Baudrate = 115200
-  Serial.flush();  // Flush buffer
+  Serial.flush();                // Flush buffer
   
   Serial.write('O');
 }
 
+// Main loop
 void loop()
 {
-  
   SCmode();
   
-  digitalWrite(7,  leftMode);
-  analogWrite (9,  leftPWM);
-  digitalWrite(8,  rightMode);
-  analogWrite (10, rightPWM);
+  digitalWrite( 7, leftMode );
+  analogWrite ( 9, leftPWM  );
+  digitalWrite( 8, rightMode);
+  analogWrite (10, rightPWM );
   
-  //  
-  //  if(leftPWM > 0)
-  //    leftPWM--;
-  //  if(rightPWM >0)
-  //    rightPWM--;  
+  //if(leftPWM > 0) {
+  //  leftPWM--;
+  //}
+  //if(rightPWM >0) {
+  //  rightPWM--;  
+  //}
 }
 
-
-
-// Discution for serial communication
-
+// Discussion for serial communication
 void SCmode()
 {
-  if( Serial.available()>=5)
+  if(Serial.available() >= 5)
   {
     int serialSecurity = Serial.read();
-    if( serialSecurity == '$')
+    if(serialSecurity == '$')
     {
-
       leftMode  = Serial.read();
       leftPWM   = Serial.read();
       rightMode = Serial.read();
       rightPWM  = Serial.read();    
     }
-
   }
 }
 
